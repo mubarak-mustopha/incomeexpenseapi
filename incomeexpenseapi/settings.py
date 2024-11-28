de90103d949c09da7cdf9d1e94907f560de69d4a
mubaarock <mustophamubarack17@gmail.com>
2024-11-28 14:00:02 +0100

    Reset

--- incomeexpenseapi/settings.py
+++ incomeexpenseapi/settings.py
@@ -85,9 +85,6 @@ DATABASES = {
 }
 
 
-REST_FRAMEWORK = {
-    "NON_FIELD_ERRORS_KEY": "error",
-}
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
