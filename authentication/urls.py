5fd56681d58bb7473297d91cd625d3437e778858
mubaarock <mustophamubarack17@gmail.com>
2024-12-04 02:50:48 +0100

    Get new access token using a refresh token

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from .views import RegisterView, VerifyEmail, LoginAPIView
 
 urlpatterns = [
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginAPIView.as_view(), name="login"),
     path("email-verify", VerifyEmail.as_view(), name="email-verify"),
